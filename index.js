bf7bd44a873c838204d259a98e7c1aaaa54d14ca
yinghaoawang <yinghao.a.wang@gmail.com>
2019-05-05 00:38:18 -0700

    base chat app created

---
+++ index.js
@@ -0,0 +1,21 @@
+let app = require('express')();
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (message) => {
+    io.emit('chat message', message);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
