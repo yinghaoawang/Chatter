e601042dc548df09f2ffe617e498c471c8a75598
yinghaoawang <yinghao.a.wang@gmail.com>
2019-05-05 02:40:38 -0700

    whispers added, all detected bugs fixed

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get('/', (req, res) => {
 
 let users = [];
 let usersTyping = [];
+let userSocketMap = [];
 
 io.on('connection', (socket) => {
   let nickname = null;
@@ -16,6 +17,9 @@ io.on('connection', (socket) => {
     if (nickname != null) {
       socket.broadcast.emit('system message', nickname + ' has disconnected');
       usersTyping.splice(usersTyping.indexOf(nickname), 1);
+      users.splice(users.indexOf(nickname), 1);
+      // todo
+      delete userSocketMap[nickname];
       io.emit('users online', users);
     }
   });
@@ -27,13 +31,29 @@ io.on('connection', (socket) => {
     }
     nickname = nick;
     users.push(nickname);
+    userSocketMap[nickname] = socket.id;
     socket.broadcast.emit('system message', nickname + ' has connected');
     io.emit('users online', users);
     socket.emit('nickname validity', true);
   });
 
   socket.on('chat message', (nickname, message) => {
-    io.emit('chat message', nickname, message);
+    if (message.length == 0) return;
+
+    if (message[0] == '/') {
+      let tokens = message.split(" ").filter(item => item);
+      if (tokens.length > 2) {
+        if (tokens[0] == '/w' && users.includes(tokens[1])) {
+            let targetSocketId = userSocketMap[tokens[1]];
+            let cleanedMsg = message.substring(2).trim();
+            cleanedMsg = cleanedMsg.substring(cleanedMsg.indexOf(' '));
+            socket.emit('system message', 'Whispered to ' + tokens[1] + ': ' + cleanedMsg);
+            socket.broadcast.to(targetSocketId).emit('system message', nickname + ' whispered ' + cleanedMsg);
+        }
+      }
+    } else {
+      io.emit('chat message', nickname, message);
+    }
   });
 
   socket.on('typing', (nickname) => {
