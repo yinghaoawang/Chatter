d8ecb2bc88dedcb1f2480d8e95e4a1823e7fe58c
yinghaoawang <yinghao.a.wang@gmail.com>
2019-05-05 01:49:31 -0700

    only unique nicknames

--- index.js
+++ index.js
@@ -6,19 +6,27 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+let users = [];
 let usersTyping = [];
 
 io.on('connection', (socket) => {
   let nickname = null;
   socket.on('disconnect', () => {
     if (nickname != null) {
-      io.emit('system message', nickname + ' has disconnected');
+      socket.broadcast.emit('system message', nickname + ' has disconnected');
+      usersTyping.splice(usersTyping.indexOf(nickname), 1);
     }
   });
 
   socket.on('set nickname', (nick) => {
+    if (users.includes(nick)) {
+      socket.emit('nickname validity', false);
+      return;
+    }
     nickname = nick;
-    io.emit('system message', nickname + ' has connected');
+    users.push(nickname);
+    socket.broadcast.emit('system message', nickname + ' has connected');
+    socket.emit('nickname validity', true);
   });
 
   socket.on('chat message', (nickname, message) => {
@@ -28,14 +36,14 @@ io.on('connection', (socket) => {
   socket.on('typing', (nickname) => {
     if (!usersTyping.includes(nickname)) {
       usersTyping.push(nickname);
-      io.emit('typing', usersTyping);
+      socket.broadcast.emit('typing', usersTyping);
     }
   });
 
   socket.on('stoptyping', (nickname) => {
     if (usersTyping.includes(nickname)) {
       usersTyping.splice(usersTyping.indexOf(nickname), 1);
-      io.emit('typing', usersTyping);
+      socket.broadcast.emit('typing', usersTyping);
     }
   });
 });
